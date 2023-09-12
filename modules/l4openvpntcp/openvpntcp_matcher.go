0bb8e1ce358f0cdd9732dfaf675023b79b698cea
tanmingxin <1121170088@qq.com>
2023-09-12 23:00:37 +0800

    update

--- modules/l4openvpntcp/openvpntcp_matcher.go
+++ modules/l4openvpntcp/openvpntcp_matcher.go
@@ -21,11 +21,11 @@ func (OpenvpnMatcher) CaddyModule() caddy.ModuleInfo {
 
 func (m OpenvpnMatcher) Match(cx *layer4.Connection) (bool, error) {
 	// read pkg length
-	length := []byte{0, 0}
-	if _, err := io.ReadFull(cx, length); err != nil {
+	lengthBs := []byte{0, 0}
+	if _, err := io.ReadFull(cx, lengthBs); err != nil {
 		return false, err
 	}
-
+	length := int(lengthBs[1]) | (int(lengthBs[0])<<8)
 	msgtype := []byte{0}
 	if _, err := io.ReadFull(cx, msgtype); err != nil {
 		return false, err
@@ -34,13 +34,45 @@ func (m OpenvpnMatcher) Match(cx *layer4.Connection) (bool, error) {
 	opcode := msgtype[0] >> 3
 
 	keyid := msgtype[0] & 0x07
-
-	if opcode < 1 || opcode > 9 {
-		return false, nil
-	}
 	if keyid != 0 {
 		return false, nil
 	}
+	if length < 14 {
+		return false, nil
+	}
+	switch opcode {
+	case 1:
+		return false, nil
+	case 2:
+		return false, nil
+	case 3:
+		return false, nil
+	case 4:
+		if length > 2000 {
+			return false, nil
+		}
+	case 5:
+		if length != 22 {
+			return false, nil
+		}
+	case 6:
+		return false, nil
+	case 7:
+		if length != 14 {
+			return false, nil
+		}
+	case 8:
+		if length != 26 {
+			return false, nil
+		}
+	case 9:
+		if length > 2000 {
+			return false, nil
+		}
+	default:
+		return false, nil
+	}
+
 	return true, nil
 }
 
